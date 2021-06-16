5e1d1f8513c3dd062f332517a4659c254c8d2005
RBOrtmann <tech.head.b@gmail.com>
2021-06-15 21:41:24 -0400

    Added initial files

---
+++ job-tracker/job.py
@@ -0,0 +1,27 @@
+"""
+Author: Brendan Ortmann
+"""
+import datetime
+
+DEFAULT_DATE = datetime.date(datetime.MINYEAR, 1, 1)
+
+class Job:
+    """A simple class that defines a Job structure."""
+
+    def __init__(self) -> None:
+        self.columns = {
+            "Company": "", 
+            "Title": "", 
+            "Date Applied": DEFAULT_DATE,
+            "Date Followed Up": DEFAULT_DATE, 
+            "Website": "", 
+            "Phone Number": "", 
+            "Location": "", 
+            "Comments": "", 
+            "Status": ""
+        }
+
+    # TODO: str implementation, concrete definition of variables (e.g. company, title, website, etc.)
+
+    def __str__(self) -> str:
+        return str(self.columns)
---
+++ job-tracker/repl.py
@@ -0,0 +1,17 @@
+"""
+Author: Brendan Ortmann
+"""
+
+# TODO: repl, add-job, delete-job, list-jobs
+
+import job
+
+# Type alias instead of relative import just to make clear what's what :)
+Job = job.Job
+
+
+
+
+example = Job()
+
+print(example)
